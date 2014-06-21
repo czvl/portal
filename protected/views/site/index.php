9f491da99ebb96b8e943f2ef9bdc065485ee8926
Maxim Shvaykovski <maxim@shvaykovski.com>
2014-06-21 14:38:23 +0300

    Fix: index.php

---
+++ protected/views/site/index.php
@@ -0,0 +1,89 @@
+        <!-- ******************** HeaderWrap ********************-->
+        <div id="headerwrap">
+            <header class="clearfix">
+                <h1><span>Ми</span> – громадянська ініціатива, покликана допомогти соціальній реабілітації та працевлаштуванню учасників новітньої української революції, а також вимушених переселенців із Криму та інших регіонів України.</h1>
+                <div class="container">
+                    <div class="row">
+                        <?php if (!IS_LOCALHOST) { ?>
+                        <div class="fb-like" data-href="http://czvl.ua-selector.in/" data-layout="standard" data-action="recommend" data-show-faces="true" data-share="true"></div>
+                        <?php } ?>
+                        <?php
+			/*<div class="span12">
+                            <ul class="icon">
+                                <!--li><a href="#" target="_blank"><i class="icon-pinterest-circled"></i></a></li-->
+                                <!--li><a href="https://www.facebook.com/groups/MaydanPulse/" target="_blank"><i class="icon-facebook-circled"></i></a></li-->
+                                <!--li><a href="#" target="_blank"><i class="icon-twitter-circled"></i></a></li-->
+                                <!--li><a href="#" target="_blank"><i class="icon-gplus-circled"></i></a></li-->
+                                <!--li><a href="#" target="_blank"><i class="icon-skype-circled"></i></a></li-->
+                            </ul>
+                        </div>*/
+			?>
+                    </div>
+                </div>
+            </header>
+        </div>
+        <!--******************** Feature ********************-->
+        <div class="scrollblock">
+            <section id="feature">
+                <div class="container">
+                    <div class="row">
+                        <div class="span12">
+                            <article>
+                                <p>Наше завдання – допомогти тим, хто внаслідок участі в акціях протесту втратили роботу і доступ до засобів існування. Тим, хто були вимушені покинути дім через події в Криму та південно-східних регіонах України.</p>
+                                <p>У базі даних &laquo;Центру зайнятості Вільних людей&raquo; – небайдужі громадяни, які шукають спосіб повернутися в русло мирного життя.</p>
+                                <p>У базі даних &laquo;Центру зайнятості Вільних людей&raquo; - небайдужі компанії, підприємці та організації, готові допомогти в працевлаштуванні тим, хто сьогодні цього потребує найдужче.</p>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!--******************** Services Section ********************-->
+        <section id="services" class="single-page scrollblock">
+            <div class="container">
+                <div class="align"><i class="icon-cog-circled"></i></div>
+                <h1>Як ви можете долучитися?</h1>
+                <div class="row">
+                    <div class="span3">
+                        <!--div class="align"> <i class="icon-desktop sev_icon"></i> </div-->
+                        <h2>Як волонтер-рекрутер:</h2>
+                        <ul>
+                            <li>Надавати індивідуальні консультації, допомагати в профорієнтації, складанні резюме;</li>
+                            <li>Працювати в точках &laquo;Центру зайнятості Вільних людей&raquo;;</li>
+                            <li>Підбирати вакансії для пошукачів до бази даних &laquo;Центру зайнятості Вільних людей&raquo;.</li>
+                        </ul>
+                        <p>Зареєструватися: <a href="http://bit.ly/czslvlntr" target="_blank">http://bit.ly/czslvlntr</a>.</p>
+                    </div>
+                    <div class="span3">
+                        <!--div class="align"> <i class="icon-vector sev_icon"></i> </div-->
+                        <h2>Як роботодавець:</h2>
+                        <ul>
+                            <li>Заповнити Анкету Роботодавця &laquo;Центру зайнятості Вільних людей&raquo; - <a href="http://bit.ly/czslcomp" target="_blank">http://bit.ly/czslcomp</a>;</li>
+                            <li>Повідомити &laquo;Центру зайнятості Вільних людей&raquo; про наявні та можливі вакансії;</li>
+                            <li>Поширити інформацію про проект у професійних спільнотах, серед друзів та колег.</li>
+                        </ul>
+                    </div>
+                    <div class="span3">
+                        <!--div class="align"> <i class="icon-basket sev_icon"></i> </div-->
+                        <h2>Як кандидат:</h2>
+                        <ul>
+                            <li>Надіслати резюме на адресу czsl.staff@gmail.com та заповнити анкету онлайн <a href="http://bit.ly/czslstaff" target="_blank">http://bit.ly/czslstaff</a>;</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--******************** Contact Section ********************-->
+        <section id="contact" class="single-page scrollblock">
+            <div class="container">
+                <div class="align"><i class="icon-mail-2"></i></div>
+                <h1>Наші контакти:</h1>
+                <div class="row">
+                    <div class="span12">
+                        <p>Для претендентів, волонтерів, а також загальні питання проекту: <strong>Наталія Швайковська</strong>, +380 (63) 241-5213, <a href="mailto:czsl.staff@gmail.com">czsl.staff@gmail.com</a>.</p>
+                        <p>Для роботодавців, професійних асоціацій, НУО: <strong>Ірина Коваль</strong>, +380 (97) 650-3125, <a href="mailto:iryna.koval1@gmail.com">iryna.koval1@gmail.com</a>.</p>
+                        <p>Група Ініціативи &laquo;<a href="https://www.facebook.com/groups/MaydanPulse/" target="_blank">Центру зайнятості Вільних людей</a>&raquo; у Facebook.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
