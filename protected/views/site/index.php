8cd98808b1513e65d83ee0edb98859b264055ac3
Maxim Shvaykovski <maxim@shvaykovski.com>
2014-07-21 18:54:38 +0300

    New text

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,7 +1,7 @@
         <!-- ******************** HeaderWrap ********************-->
         <div id="headerwrap">
             <header class="clearfix">
-                <h1><span>Ми</span> – громадянська ініціатива, покликана допомогти соціальній реабілітації та працевлаштуванню учасників новітньої української революції, а також вимушених переселенців із Криму та інших регіонів України.</h1>
+                <h1><span>Ми</span> – громадська ініціатива, покликана сприяти працевлаштуванню та соціальній реабілітації громадян України, які у зв’язку з політичними репресіями, військовими діями та складною економічною ситуацією втратили роботу та потребують допомоги.</h1>
                 <div class="container">
                     <div class="row">
                         <?php if (!IS_LOCALHOST) { ?>
@@ -111,10 +111,11 @@
                 <h1>Наші контакти:</h1>
                 <div class="row">
                     <div class="span12">
-                        <p>Для претендентів, волонтерів, а також загальні питання проекту: <strong>Наталія Швайковська</strong>, +380 (63) 241-5213, <a href="mailto:czsl.staff@gmail.com">czsl.staff@gmail.com</a>.</p>
-                        <p>Для роботодавців, професійних асоціацій, НУО: <strong>Ірина Коваль</strong>, +380 (97) 650-3125, <a href="mailto:iryna.koval1@gmail.com">iryna.koval1@gmail.com</a>.</p>
-                        <p>Рекомендуємо підписатись на нашу сторінку в Facebook, щоб слідкувати за новинами і вакансіями: <a href="https://www.facebook.com/czvl.staff" target="_blank">https://www.facebook.com/czvl.staff</a></p>
-                        <p>Телефон гарячої лінії: <strong>+380 (67) 507-0930</strong>.</p>
+                        <p>Гаряча лінія для претендентів: +380 (67) 507-09-30 (<strong>Kyivstar</strong>), +380 (66) 500-35-75 (<strong>MTS</strong>), +380 (93) 167-37-81 (<strong>Life</strong>), <a href="mailto:czsl.staff@gmail.com">czsl.staff@gmail.com</a>.</p>
+                        <p>Для роботодавців: <strong>Людмила Перевертень</strong>, +38 (97) 014-59-49, <a href="mailto:czvl.job@gmail.com">czvl.job@gmail.com</a>.</p>
+                        <p>Для професійних асоціацій, громадських організацій та ініціатив: <strong>Ірина Коваль</strong>, +380 (97) 650-31-25, <a href="mailto:iryna.koval1@gmail.com">iryna.koval1@gmail.com</a>.</p>
+                        <p>Загальні питання проекту, digital-частина: <strong>Наталія Швайковська</strong>, +380 (63) 241-52-13, <a href="mailto:shvaykovska@gmail.com">shvaykovska@gmail.com</a>.</p>
+                        <p>Рекомендуємо підписатись на нашу сторінку в Facebook, щоб слідкувати за новинами і вакансіями: <a href="https://www.facebook.com/czvl.staff" target="_blank">https://www.facebook.com/czvl.staff</a>.</p>
                     </div>
                 </div>
             </div>
