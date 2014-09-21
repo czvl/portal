66ada7ac7a2261af64888ff9136dbd1c556c5433
Maxim Shvaykovski <maxim@shvaykovski.com>
2014-09-21 10:48:12 +0300

    Fix: filter dropdown menu

--- js/inside.js
+++ js/inside.js
@@ -1,24 +1,23 @@
 jQuery.fn.filterByText = function() {
     return this.each(function() {
         var input = this;
-        var select = $(input).next('div').children('span');
         var labels = [];
-        $(select).children('label').each(function() {
+        var select = $(input).next('div').children('span');
+
+        $(select).children('label').each(function(i) {
             labels.push(this);
         });
-        
-        $(select).data('labels', labels);
+
         $(input).bind('change keyup', function() {
-            var labels = $(select).empty().scrollTop(0).data('labels');
             var search = $(this).val().trim();
             var regex = new RegExp(search, "gi");
-            
+
             $.each(labels, function(i) {
                 var label = labels[i];
-                if ($(label).text().match(regex) !== null) {
-                    $(select).append(
-                        label
-                    );
+                if ($(label).text().match(regex) === null) {
+                    $(label).hide();
+                } else {
+                    $(label).show();
                 }
             });
         });
