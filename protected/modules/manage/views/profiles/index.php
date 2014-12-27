5e88f4d1c037a5ccfdc4b45e95dc62e7da568bc8
Maxim Shvaykovski <maxim@shvaykovski.com>
2014-12-27 15:48:44 +0200

    Profiles filters - show selected

--- protected/modules/manage/views/profiles/index.php
+++ protected/modules/manage/views/profiles/index.php
@@ -3,6 +3,29 @@
 $this->menu = array(
     array('label' => 'Додати анкету', 'url' => array('create')),
 );
+
+$statusFilter           = $this->getVariable('status');
+$lastNameFilter         = $this->getVariable('last_name');
+$firstNameFilter        = $this->getVariable('first_name');
+$internalNumFilter      = $this->getVariable('internal_num');
+$recruiterIdFilter      = $this->getVariable('recruiter_id');
+$locationsFilter        = $this->getVariable('locations');
+$residenciesFilter      = $this->getVariable('residencies');
+$categoriesFilter       = $this->getVariable('categories');
+$positionsFilter        = $this->getVariable('positions');
+$assistanceIdsFilter    = $this->getVariable('assistanceIds');
+$licensesIdsFilter      = $this->getVariable('licensesIds');
+
+function getClassName($fieldValue = NULL)
+{
+    return ($fieldValue != NULL) ? 'selected' : 'default';
+}
+
+function getOrder($fieldValue, $orderField = 'id')
+{
+    return ($fieldValue) ? $orderField . " IN (" . implode(",", $fieldValue) . ") DESC," : "";
+}
+
 ?>
 
 <h1>Анкети претендентів</h1>
@@ -15,61 +38,61 @@ $this->menu = array(
                 <td>
                     <div class="scrollY">
                         <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('status')); ?></strong><br />
-                        <?php echo CHtml::dropDownList('status', $this->getVariable('status'), CvList::model()->getStatusTypes(), array('empty' => '---')); ?>
+                        <?php echo CHtml::dropDownList('status', $statusFilter, CvList::model()->getStatusTypes(), array('empty' => '---', 'class' => getClassName($statusFilter))); ?>
                         <br />
                         <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('last_name')); ?></strong><br />
-                        <?php echo CHtml::textField('last_name', $this->getVariable('last_name'), array('span' => 5, 'maxlength' => 255)); ?>
+                        <?php echo CHtml::textField('last_name', $lastNameFilter, array('span' => 5, 'maxlength' => 255, 'class' => getClassName($lastNameFilter))); ?>
                         <br />
                         <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('first_name')); ?></strong><br />
-                        <?php echo CHtml::textField('first_name', $this->getVariable('first_name'), array('span' => 5, 'maxlength' => 255)); ?>
+                        <?php echo CHtml::textField('first_name', $firstNameFilter, array('span' => 5, 'maxlength' => 255, 'class' => getClassName($firstNameFilter))); ?>
                         <br />
                         <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('internal_num')); ?></strong><br />
-                        <?php echo CHtml::textField('internal_num', $this->getVariable('internal_num'), array('span' => 5, 'maxlength' => 255)); ?>
+                        <?php echo CHtml::textField('internal_num', $internalNumFilter, array('span' => 5, 'maxlength' => 255, 'class' => getClassName($internalNumFilter))); ?>
                         <br />
                         <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('recruiter_id')); ?></strong><br />
-                        <?php echo CHtml::dropDownList('recruiter_id', $this->getVariable('recruiter_id'), User::model()->recruiters, array('empty' => '---')); ?>
+                        <?php echo CHtml::dropDownList('recruiter_id', $recruiterIdFilter, User::model()->recruiters, array('empty' => '---', 'class' => getClassName($recruiterIdFilter))); ?>
                     </div>
                 </td>
-                <td>
+                <td class="<?php echo getClassName($locationsFilter); ?>">
                     <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('jobLocationsIds')); ?></strong><br />
                     <input type="text" name="locationsFilter" class="filter" size="10" />
                     <div class="div-overflow narrow">
-                        <?php echo CHtml::checkBoxList('locations', $this->getVariable('locations'), CHtml::listData(CitiesList::model()->findAll(array('order' => 'city_name')), 'city_index', 'city_name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
+                        <?php echo CHtml::checkBoxList('locations', $locationsFilter, CHtml::listData(CitiesList::model()->findAll(array('order' => getOrder($locationsFilter, 'city_index') . 'city_name ASC')), 'city_index', 'city_name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
                     </div>
                 </td>
-                <td>
+                <td class="<?php echo getClassName($residenciesFilter); ?>">
                     <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('residenciesIds')); ?></strong><br />
                     <input type="text" name="residenciesFilter" class="filter" size="10" />
                     <div class="div-overflow narrow">
-                        <?php echo CHtml::checkBoxList('residencies', $this->getVariable('residencies'), CHtml::listData(CitiesList::model()->findAll(array('order' => 'city_name')), 'city_index', 'city_name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
+                        <?php echo CHtml::checkBoxList('residencies', $residenciesFilter, CHtml::listData(CitiesList::model()->findAll(array('order' => getOrder($residenciesFilter, 'city_index') . 'city_name ASC')), 'city_index', 'city_name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
                     </div>
                 </td>
-                <td>
+                <td class="<?php echo getClassName($categoriesFilter); ?>">
                     <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('categoryIds')); ?></strong><br />
                     <input type="text" name="categoryFilter" class="filter" size="10" />
                     <div class="div-overflow narrow">
-                        <?php echo CHtml::checkBoxList('categories', $this->getVariable('categories'), CHtml::listData(CvCategories::model()->findAll(array('order' => 'name')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
+                        <?php echo CHtml::checkBoxList('categories', $categoriesFilter, CHtml::listData(CvCategories::model()->findAll(array('order' => getOrder($categoriesFilter) . 'name ASC')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
                     </div>
                 </td>
-                <td>
+                <td class="<?php echo getClassName($positionsFilter); ?>">
                     <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('positionsIds')); ?></strong><br />
                     <input type="text" name="positionsFilter" class="filter" size="10" />
                     <div class="div-overflow narrow">
-                        <?php echo CHtml::checkBoxList('positions', $this->getVariable('positions'), CHtml::listData(CvPositions::model()->findAll(array('order' => 'name')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
+                        <?php echo CHtml::checkBoxList('positions', $positionsFilter, CHtml::listData(CvPositions::model()->findAll(array('order' => getOrder($positionsFilter) . 'name ASC')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
                     </div>
                 </td>
-                <td>
+                <td class="<?php echo getClassName($assistanceIdsFilter); ?>">
                     <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('assistanceIds')); ?></strong><br />
                     <input type="text" name="assistanceFilter" class="filter" size="10" />
                     <div class="div-overflow narrow">
-                        <?php echo CHtml::checkBoxList('assistanceIds', $this->getVariable('assistanceIds'), CHtml::listData(AssistanceTypes::model()->findAll(array('order' => 'name')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
+                        <?php echo CHtml::checkBoxList('assistanceIds', $assistanceIdsFilter, CHtml::listData(AssistanceTypes::model()->findAll(array('order' => getOrder($assistanceIdsFilter) . 'name ASC')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
                     </div>
                 </td>
-                <td>
+                <td class="<?php echo getClassName($licensesIdsFilter); ?>">
                     <strong><?php echo CHtml::encode(CvList::model()->getAttributeLabel('driverLicensesIds')); ?></strong><br />
                     <input type="text" name="licensesFilter" class="filter" size="10" />
                     <div class="div-overflow narrow">
-                        <?php echo CHtml::checkBoxList('licensesIds', $this->getVariable('licensesIds'), CHtml::listData(DriverLicenses::model()->findAll(array('order' => 'name')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
+                        <?php echo CHtml::checkBoxList('licensesIds', $licensesIdsFilter, CHtml::listData(DriverLicenses::model()->findAll(array('order' => getOrder($licensesIdsFilter) . 'name ASC')), 'id', 'name'), array('template' => '{beginLabel}{input} {labelTitle}{endLabel}', 'separator' => '')); ?>
                     </div>
                 </td>
             </tr>
