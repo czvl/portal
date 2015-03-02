361e7ed83a7e3cec13e5173e8b2d9579155f064c
Maxim Shvaykovski <maxim@shvaykovski.com>
2015-03-02 21:31:03 +0200

    Moved path to local Yii folder from index.php to local.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+date_default_timezone_set('Europe/Kiev');
+
+$isLocalhost = (filter_input(INPUT_SERVER, 'SERVER_ADDR') == '127.0.0.1' ? true : false);
+
+defined('IS_LOCALHOST') or define('IS_LOCALHOST', $isLocalhost);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
+$protected  = __DIR__ . '/protected';
+$config     = $protected . '/config/main.php';
+$yii        = $protected . '/vendor/yii/framework/yii.php';
+
+if (file_exists($protected . '/local.php')) {
+	require_once $protected . '/local.php';
+}
+
+require_once $yii;
+Yii::createWebApplication($config)->run();
