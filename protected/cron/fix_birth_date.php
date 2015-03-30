0010abb7f036d84007e7dda8db5ef4ba0c4c4bed
Maxim Shvaykovski <maxim@shvaykovski.com>
2015-03-30 16:11:28 +0300

    Update path in cron fix

--- protected/cron/fix_birth_date.php
+++ protected/cron/fix_birth_date.php
@@ -2,8 +2,9 @@
 
 $sql = "UPDATE cv_list SET birth_date = NULL WHERE (DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), birth_date)), '%Y')+0) < 10";
 
-$config = include_once '../config/db.php';
+$config = include_once '/var/www/sites/czvl.org.ua/www/protected/config/db.php';
 
 $pdo = new PDO($config['connectionString'], $config['username'], $config['password']);
 $fix = $pdo->prepare($sql);
 $fix->execute();
+
