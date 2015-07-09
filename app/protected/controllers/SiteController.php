0d7f01acac7b8f81dd1e97e2e177942bb97686d1
spartaksun <2697024@gmail.com>
2015-07-09 22:09:39 +0300

    #2 Test mail action 2

--- app/protected/controllers/SiteController.php
+++ app/protected/controllers/SiteController.php
@@ -218,15 +218,8 @@ class SiteController extends Controller
 
     public function actionTest()
     {
-        $message = new YiiMailMessage;
-        $message->setBody(Yii::t('main', 'user.email.confirm.body', [
-            ':link' => Yii::app()->createAbsoluteUrl('/site/confirm_email', [
-                // 'hash' => $user->hash
-            ]),
-        ]), 'text/html');
-        $message->subject = Yii::t('main', 'user.email.confirm.subject');
-        $message->addTo('2697024@gmail.com');
-        $message->from = Yii::app()->params['noreply@czvl.org.ua'];
-        Yii::app()->mail->send($message);
+
+        mail('2697024@gmail.com', 'test subject', 'test message');
+        echo "OK";
     }
 }
