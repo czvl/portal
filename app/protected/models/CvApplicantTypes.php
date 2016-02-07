76bba80a25b8a21cbce75ac8f92119421cdc305d
asopin <alexey.sopin@gmail.com>
2016-02-07 12:28:33 +0200

    #32 migration and models created

---
+++ app/protected/models/CvApplicantTypes.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This is the model class for table "cv_applicant_types".
+ *
+ * The followings are the available columns in table 'cv_applicant_types':
+ * @property integer $id
+ * @property string $name
+ *
+ * The followings are the available model relations:
+ * @property CvList[] $cvLists
+ */
+class CvApplicantTypes extends CActiveRecord
+{
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'cv_applicant_types';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('name', 'required'),
+            array('name', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, name', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'cvLists' => array(self::MANY_MANY, 'CvList', 'cv_to_applicant_type(applicant_type_id, cv_id)'),
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'name' => 'Name',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
+
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('id', $this->id);
+        $criteria->compare('name', $this->name, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return CvApplicantTypes the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+}
